e598b3b22292d490d5cba9a73f8f717bf133a6bd
Mike King <mike@85pixels.com>
2014-04-20 17:03:17 -1000

    Added scss version of vendor assets

--- app/scripts/controllers/navigation.js
+++ app/scripts/controllers/navigation.js
@@ -1,10 +1,14 @@
 'use strict';
 
 angular.module('mintpalMarketApp')
-  .controller('NavigationCtrl', function ($scope) {
+  .controller('NavigationCtrl', function ($scope, cfpLoadingBar) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $scope.start = function() {
+      cfpLoadingBar.start();
+    };
   });
