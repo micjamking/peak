ab7fa9466ae6a49b0d047f2bd3ef6d5ab8278e4c
Mike King <mike@85pixels.com>
2014-04-20 22:16:21 -1000

    Added sorting for columns

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,11 +3,21 @@
 angular.module('mintpalMarketApp').controller('MainCtrl', function ($scope, $http, $interval) {
 
 	var url = 'https://api.mintpal.com/v1/market/summary/BTC';
+	
+	var dataProcessing = function(array){
+		var i;
+
+		for (i = 0; i < array.length; i++){
+			array[i]["change"] = +(parseFloat(array[i]["change"]).toFixed(2));
+		}
+		
+		return array;
+	};
 
 	$scope.getMarket = function(){
 		$http.jsonp('http://whateverorigin.org/get?url=' + encodeURIComponent(url) + '&callback=JSON_CALLBACK').success(function(data){
-			$scope.data = angular.fromJson(data.contents);
-			//console.log($scope.data);
+			$scope.data = dataProcessing(angular.fromJson(data.contents));
+			console.log(dataProcessing($scope.data));
 		});
 	};
 
