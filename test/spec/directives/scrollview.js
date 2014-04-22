68689a18182a088bb99a5a5419ee095c1dcb21c6
Mike King <mike@85pixels.com>
2014-04-21 15:13:25 -1000

    Added new directive for inner scroll view

--- test/spec/directives/scrollview.js
+++ test/spec/directives/scrollview.js
@@ -5,14 +5,13 @@ describe('Directive: scrollView', function () {
   // load the directive's module
   beforeEach(module('peakApp'));
 
-  var element,
-    scope;
+  var scope;
 
   beforeEach(inject(function ($rootScope) {
     scope = $rootScope.$new();
   }));
 
-  it('should make hidden element visible', inject(function ($compile) {
+  it('should make hidden element visible', inject(function () {
     //element = angular.element('<scroll-view></scroll-view>');
     //element = $compile(element)(scope);
     //expect(element.text()).toBe('this is the scrollView directive');
