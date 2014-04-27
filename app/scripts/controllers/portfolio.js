a9c154d0083aea7fa933aa2bbd7f31efcc663ae2
Mike King <mike@85pixels.com>
2014-04-26 21:31:07 -1000

    Added calculations for portfolio

--- app/scripts/controllers/portfolio.js
+++ app/scripts/controllers/portfolio.js
@@ -3,12 +3,70 @@
    ========================================================================== */
 'use strict';
 
-angular.module('peakApp').controller('PortfolioCtrl', function ($scope, localStorageService) {
+angular.module('peakApp').controller('PortfolioCtrl', function ($scope, $http, localStorageService) {
   
+  // Portfolio Module
   $scope.portfolio = (function(){
-
-    return {
+    
+    // Private Variables
+    var TODAY     = new Date();
+    var ONE_WEEK  = (60 * 60 * 24 * 7) * 1000;
+    var URL       = 'https://api.mintpal.com/v1/market/summary/BTC';
+    var PROXY     = 'http://glacial-chamber-5485.herokuapp.com/?url=';
+    var total, holdings, addHolding, getMarket, coins;
+    
+    var storage = {
+      set: localStorageService.add,
+      get: localStorageService.get,
+      remove: localStorageService.remove,
+      clear: localStorageService.clearAll
+    };
+    
+    // Grab total from localStorage or set total to 0
+    total = function(){
+      var i, amount = 0;
       
+      for (i = 0; i < holdings.length; i++){
+        amount += holdings[i].amount*holdings[i].cost;
+      }
+      
+      return amount;
+    };
+    
+    holdings = storage.get('holdings') || [];
+
+    // Private Function
+    var getMarket = function(){
+
+        // API Call
+        $http.get(PROXY + encodeURIComponent(URL)).success(function(data){
+
+            // Process response & store in $scope property
+            data.push({})
+            $scope.data = data;
+            storage.set('coins', data);
+            storage.set('timestamp', TODAY.getTime());
+        });
+    };
+    
+    // Private Function
+    var addHolding = function() {
+      holdings.push({coin:$scope.coin.code, amount:$scope.amount, cost:$scope.cost});
+      storage.set('holdings', holdings);
+      $scope.amount = '';
+      $scope.cost = '';
+    };
+    
+    if ((TODAY.getTime() - storage.get('timestamp')) >= ONE_WEEK) {
+      getMarket();
+    } else {
+      $scope.data = storage.get('coins');
+    }
+    
+    return {
+      totalValue: total,
+      holdings: holdings,
+      addHolding: addHolding
     };
   })();
 });
